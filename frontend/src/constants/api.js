daf62280e0be81b3d24c51cbb5f7a47ce20030f8
gourav-017 <gouravrajput23110@gmail.com>
2025-12-12 16:23:16 +0530

    commit

--- frontend/src/constants/api.js
+++ frontend/src/constants/api.js
@@ -1,4 +1,4 @@
-export const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+export const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api' || "https://smartlead-wss5.onrender.com/api";
 
 export const API_ENDPOINTS = {
   LEADS: '/leads',
